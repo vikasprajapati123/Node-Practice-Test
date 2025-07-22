409d022f5ca0b6fe0887c356fc7ddf809ced10de
Vikas <vikas.xornor@gmail.com>
2025-07-22 22:43:27 +0530

    sdf

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 require('dotenv').config();
 const db = require('./db.js');
 
-console.log("click")
+console.log("click");
 app.get('/',(req,res)=>{
     res.status(200).json({
         success: true,
